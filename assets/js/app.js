3c57a8020d014476c186e8744fa901bf3aaef60a
StasTikh <tmihatelefan@gmail.com>
2021-12-08 12:35:42 +0200

    new version

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,8 @@
 
-import { showBarrows, showOws } from "../js/func.js";
+//import { showBarrows, showOws } from "../js/func.js";
+
+const URL = 'https://bank.gov.ua/NBUStatService/v1/statdirectory/ovdp?json';
+const ratesURL = 'https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json';
 
 import { createApp } from "../../node_modules/vue/dist/vue.esm-browser.prod.js";
 
@@ -8,14 +11,92 @@ createApp({
         return {
             title1: "Period #1",
             title2: "Period #2",
-            date1per1: "",
-            date2per1: "",
-            date1per2: "",
-            date2per2: ""
+            periodOne: {dateStart: "",
+                        dateFinish: ""},
+            dataArr: [],
+            currency: [],
+
         }
     },
+
     computed: {
-        periodOneBarrows() {
+        borrowedSum(){
+            
+            function calc(date1, date2, arr){
+                let sum = 0;
+                    for (let item of arr){
+                        if(item.paydate >= date1 && item.repaydate <= date2){
+                            sum += item.attraction;
+                        }
+                    }
+                    return sum;
+            }
+            return calc(this.periodOne.dateStart, this.periodOne.dateFinish, this.dataArr); 
+        },
+
+        owedSum(){
+            function calc(date1, date2, arr){
+                let sum = 0;
+                    for (let item of arr){
+                        if(item.paydate >= date1 && item.repaydate > date2){
+                            sum += item.attraction;
+                        }
+                    }
+                    return sum;
+            }
+            return calc(this.periodOne.dateStart, this.periodOne.dateFinish, this.dataArr); 
+        }
+    },
+    async mounted(){
+        
+        let rates = await fetch(ratesURL);
+        rates = await rates.json();
+        this.currency = rates;
+
+
+        let data = await fetch(URL);
+        data = await data.json();
+
+        data = data.map(item => {
+            return {
+                'paydate': item.paydate, 
+                'repaydate': item.repaydate,
+                'attraction': item.attraction,
+                'valcode': item.valcode 
+            } 
+        });
+
+        function swap(massive) {
+            if (massive.length < 2) {
+                return massive;
+            } 
+                    let tmp = massive[0];
+                    massive[0] = massive[massive.length-1];
+                    massive[massive.length-1] = tmp;
+                    return massive;
+        };
+
+        for(let item of data) {
+            for(let rate of this.currency) {
+                if(item.valcode == rate.cc) {
+                    item.attraction = item.attraction * rate.rate;
+                    item.valcode = rate.cc + " in UAH";
+                }
+            };
+            item.attraction = Math.round(item.attraction);
+            item.paydate = item.paydate.split('.');
+            item.repaydate = item.repaydate.split('.');
+            item.paydate = swap(item.paydate).join('-');
+            item.repaydate = swap(item.repaydate).join('-');
+        };
+        this.dataArr = data;
+        console.log(this.dataArr, this.currency);
+    }
+}).mount('#app');
+
+
+/*
+periodOneBarrows() {
             return showBarrows(this.date1per1, this.date2per1);
         },
         periodOneOws() {
@@ -40,5 +121,4 @@ createApp({
                 return Math.round(((this.periodTwoOws - this.periodOneOws) / this.periodOneOws) * 100);
             }
         }
-    }
-}).mount('#app');
+*/
--- assets/js/func.js
+++ assets/js/func.js
@@ -7,7 +7,7 @@
     let data = await fetch(URL);
     data = await data.json();
 
-    console.log(data)
+    //console.log(data)
 
     data = data.map(item => {
         return {
@@ -26,98 +26,69 @@
             }
         };
         item.attraction = Math.round(item.attraction);
+        item.attraction = Math.round(item.attraction);
+        item.paydate = item.paydate.split('.');
+        item.repaydate = item.repaydate.split('.');
+        item.paydate = swap(item.paydate).join('-');
+        item.repaydate = swap(item.repaydate).join('-');
         
     };
 
-    //console.log(data)
+    function swap(massive) {
+        if (massive.length < 2) {
+            return massive;
+        } 
+                let tmp = massive[0];
+                massive[0] = massive[massive.length-1];
+                massive[massive.length-1] = tmp;
+                return massive;
+    };
+
+    console.log(data)
 
 
     function showBarrows(date1, date2) {
 
-        date1 = date1.split('-');
-        date2 = date2.split('-');
-        
-        function swap(massive) {
-            if (massive.length < 2) {
-                return massive;
-            } 
-                    let tmp = massive[0];
-                    massive[0] = massive[massive.length-1];
-                    massive[massive.length-1] = tmp;
-                    return massive;
-        };
-
-        date1 = swap(date1).join('-');
-        date2 = swap(date2).join('-');
-        
-        const parse = dateString => {
-            const [d, m, y] = dateString.split('-');
-            return new Date(y, m-1, d)
-        };
+        // const parse = dateString => {
+        //     const [d, m, y] = dateString.split('-');
+        //     return new Date(y, m-1, d)
+        // };
 
 
-        date1 = parse(date1);
-        date2 = parse(date2);
+        // date1 = parse(date1);
+        // date2 = parse(date2);
 
         let borrowed = 0;
         let owed = 0;
         
         for(let item of data) {
-            if(new Date(item.paydate) >= date1 && new Date(item.repaydate) <= date2){
+            if(item.paydate >= date1 && item.repaydate <= date2){
                 borrowed += item.attraction;
             
-            } else if (new Date(item.paydate) >= date1 && new Date(item.repaydate) > date2) {
+            } else if (item.paydate >= date1 && item.repaydate > date2) {
                 owed += item.attraction;
             };
         };
-        return borrowed;
+        return {borrowed, date1, date2};
         //return {date1, date2}
     };
 
     function showOws(date1, date2) {
-        
-        date1 = date1.split('-');
-        date2 = date2.split('-');
-        
-        function swap(massive) {
-            if (massive.length < 2) {
-                return massive;
-            } 
-                    let tmp = massive[0];
-                    massive[0] = massive[massive.length-1];
-                    massive[massive.length-1] = tmp;
-                    return massive;
-        };
-
-        date1 = swap(date1).join('-');
-        date2 = swap(date2).join('-');
-        
-        const parse = dateString => {
-            const [d, m, y] = dateString.split('-');
-            return new Date(y, m-1, d)
-        };
-        date1 = parse(date1);
-        date2 = parse(date2);
-        
         let borrowed = 0;
         let owed = 0;
         
         for(let item of data) {
-            if(new Date(item.paydate) >= date1 && new Date(item.repaydate) <= date2){
+            if(item.paydate >= date1 && item.repaydate <= date2){
                 borrowed += item.attraction;
             
-            } else if (new Date(item.paydate) >= date1 && new Date(item.repaydate) > date2) {
+            } else if (item.paydate >= date1 && item.repaydate > date2) {
                 owed += item.attraction;
             }
         }; 
-        return owed;
+        return {owed, date1, date2};
     }
 
+    console.log(showBarrows("2017-02-03", "2021-12-08"))
+    console.log(showOws("2017-02-03", "2021-12-08"))
+
     export {showBarrows, showOws};
-
-    // let x = showBarrows('2017-01-01', '2021-12-31');
-    // let y = showBarrows('2009-09-21', '2014-08-12');
-
-    // let z = Math.round(((y - x) / x) * 100)
-
-    // console.log(`X: ${x}, Y: ${y}, Z: ${z}`)
